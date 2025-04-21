e4ec78bd24d9de31ba7655dd5c9584f53035faee
Rayan Correa <111525526+raycorrea@users.noreply.github.com>
2025-04-21 15:10:14 -0300

    Add files via upload

---
+++ Sabor-Express/app.py
@@ -0,0 +1,54 @@
+import os
+
+def exibir_nome_do_programa():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    # os.system('clear') 
+    print('Finalizando o app')
+
+def opcao_invalida():
+    print('Opção Invalida!\n')
+    input('Digite uma Tecla para Voltar ao menu Principal')
+    main()
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # opcao_escolhida = int(opcao_escolhida)
+
+    try:
+        if opcao_escolhida == 1: 
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2: 
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else: 
+            opcao_invalida()
+    except:
+        opcao_invalida
+
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
